ab291af227bbf8e6a4ebc0d9484d9e9f31756de9
Andrea Manzi <manzolo@libero.it>
2024-03-03 09:50:49 +0100

    Init

--- create.sh
+++ create.sh
@@ -16,8 +16,8 @@ apt-get install -y bridge-utils
 BRIDGE_NAME="manzolo-br0"
 INTERFACE_NAME="eno2"
 TAP_PREFIX="manzolo-tap"
-VM_PREFIX="pve0"
 NUM_VMS=3
+VM_PREFIX="pve0"
 
 # Crea il bridge
 brctl addbr "$BRIDGE_NAME"
@@ -48,35 +48,10 @@ done
 # Configura il bridge per l'accesso alla rete, ad esempio tramite DHCP
 dhclient -v "$BRIDGE_NAME"
 
-# Creazione delle macchine virtuali
+# Creazione dei dischi per la macchina virtuale
 for ((i = 1; i <= NUM_VMS; i++)); do
-    # Creazione dei dischi per la macchina virtuale
     qemu-img create -f qcow2 -o preallocation=off "${VM_PREFIX}${i}a1.qcow2" 20G
     qemu-img create -f qcow2 -o preallocation=off "${VM_PREFIX}${i}a2.qcow2" 20G
     qemu-img create -f qcow2 -o preallocation=off "${VM_PREFIX}${i}b1.qcow2" 40G
     qemu-img create -f qcow2 -o preallocation=off "${VM_PREFIX}${i}b2.qcow2" 40G
-    
-    # Creazione della macchina virtuale
-    echo "Creazione di VM${i}..."
-    qemu-system-x86_64 \
-    -name "${VM_PREFIX}${i}" \
-    -machine pc-q35-7.0,usb=off,vmport=off,dump-guest-core=off \
-    -accel kvm \
-    -cpu host,migratable=on \
-    -drive file="${VM_PREFIX}${i}a1.qcow2",index=0,media=disk \
-    -drive file="${VM_PREFIX}${i}a2.qcow2",index=1,media=disk \
-    -drive file="${VM_PREFIX}${i}b1.qcow2",index=2,media=disk \
-    -drive file="${VM_PREFIX}${i}b2.qcow2",index=3,media=disk \
-    -m 2048 \
-    -boot order=cd \
-    -drive file=proxmox-ve_8.1-2.iso,format=raw,if=none,id=cdrom \
-    -device virtio-scsi-pci \
-    -device scsi-cd,drive=cdrom \
-    -netdev "tap,id=net${i}",ifname="${TAP_PREFIX}${i}",script=no,downscript=no \
-    -device e1000,netdev=net${i},mac=52:55:00:d1:55:0${i} \
-    -smp cores=2 \
-    -vga std \
-    -display gtk &
 done
-
-echo "Le VM sono in esecuzione."
